8c46978d1bf7af4e89eebb4e6b21c05cf0eadb92
Akshay Sharma <akshayne912@gmail.com>
2023-07-17 23:04:35 +0530

    second commit

--- public/app.js
+++ public/app.js
@@ -1,48 +1,45 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Find the form element
-    const form = document.querySelector('form');
-  
-    // Add event listener to the form's submit event
-    form.addEventListener('submit', function(event) {
-      event.preventDefault(); // Prevent form submission
-  
-      // Get form input values
-//       const email = document.querySelector('input[name="email"]').value;
-// const subject = document.querySelector('input[name="subject"]').value;
-// const message = document.querySelector('textarea[name="message"]').value;
+  // Find the form element
+  const form = document.querySelector('form');
 
-      const email = document.querySelector('#emaill').value;
-      const subject = document.querySelector('#sub').value;
-      const message = document.querySelector('#mssg').value;
-    console.log(email);
-    console.log(subject)
-      // Create an object with the form data
-      const formData = {
-        email: email,
-        subject: subject,
-        message: message
-      };
-  console.log(formData);
-      // Send the form data to the server
-      fetch('/send-email', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formData)
+  // Add event listener to the form's submit event
+  form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+
+    // Get form input values
+    const email = document.querySelector('#emaill').value;
+    const subject = document.querySelector('#sub').value;
+    const message = document.querySelector('#mssg').value;
+
+    // Create an object with the form data
+    const formData = {
+      email: email,
+      subject: subject,
+      message: message
+    };
+
+    // Send the form data to the server
+    fetch('/send-email', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+      .then(function(response) {
+        if (response.ok) {
+          // Email sent successfully
+          console.log('Email sent successfully');
+          
+          // Redirect the user to a success page
+          window.location.href ="./success.html";
+        } else {
+          // Error sending email
+          console.error('Error sending email');
+        }
       })
-        .then(function(response) {
-          if (response.ok) {
-            // Email sent successfully
-            console.log('Email sent successfully');
-          } else {
-            // Error sending email
-            console.error('Error sending email');
-          }
-        })
-        .catch(function(error) {
-          console.error('Error sending email:', error);
-        });
-    });
+      .catch(function(error) {
+        console.error('Error sending email:', error);
+      });
   });
-  
+});
