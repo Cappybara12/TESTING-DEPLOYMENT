3d799dc801c362dffbb90263972fa0ea94224b2b
Akshay Kumar Sharma <akshayne912@gmail.com>
2023-12-29 03:20:31 +0530

    Update servesmail.js

--- servesmail.js
+++ servesmail.js
@@ -19,8 +19,8 @@ const transporter = nodemailer.createTransport({
     port: 465,
     secure: true,
     auth: {
-      user: 'akshayne911@gmail.com',
-      pass: 'fvbeamzmpdmnskva'
+      user: process.env.EMAIL,
+      pass: process.env.APP_PASSWORD
   
     }
   });
